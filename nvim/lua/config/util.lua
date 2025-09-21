45170d95f3fda34cab9ab30e001d44e4f66afd2d
WEI LI <limijd@protonmail.com>
2025-09-21 21:38:44 +0800

    Adjust vendor path helper for new hierarchy

--- nvim/lua/config/util.lua
+++ nvim/lua/config/util.lua
@@ -1,9 +1,23 @@
 local M = {}
 
+local uv = vim.uv or vim.loop
 local config_root = vim.fn.stdpath("config")
 
+local function dir_exists(path)
+  local stat = uv.fs_stat(path)
+  return stat and stat.type == "directory"
+end
+
+local vendor_root = config_root .. "/vendor/plugins"
+if not dir_exists(vendor_root) then
+  local repo_vendor = config_root .. "/../vendor/plugins"
+  if dir_exists(repo_vendor) then
+    vendor_root = repo_vendor
+  end
+end
+
 function M.vendor(name)
-  return vim.fs.normalize(config_root .. "/vendor/plugins/" .. name)
+  return vim.fs.normalize(vendor_root .. "/" .. name)
 end
 
 return M
