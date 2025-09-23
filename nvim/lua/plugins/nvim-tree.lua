0e9e71d846459e6d046d683bfbafbcf94721edda
WEI LI <limijd@protonmail.com>
2025-09-23 23:12:08 +0800

    Restore nvim-tree plugin without auto-open

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,38 @@
+local util = require("config.util")
+
+return {
+  name = "nvim-tree.lua",
+  dir = util.vendor("nvim-tree.lua"),
+  dependencies = {
+    "nvim-web-devicons",
+  },
+  cmd = {
+    "NvimTreeToggle",
+    "NvimTreeFocus",
+    "NvimTreeFindFile",
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("nvim-tree.api").tree.toggle({ focus = false })
+      end,
+      desc = "Toggle file explorer",
+    },
+  },
+  config = function()
+    require("nvim-tree").setup({
+      disable_netrw = false,
+      hijack_netrw = false,
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+      view = {
+        width = 35,
+      },
+    })
+  end,
+}
