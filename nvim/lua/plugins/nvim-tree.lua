0b27da5f3a7fb0cdede817413ff2c86ae81318c6
WEI LI <limijd@protonmail.com>
2025-09-23 22:49:23 +0800

    fix: auto-open nvim-tree when editing directories

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,37 @@ return {
   dependencies = {
     "nvim-web-devicons",
   },
+  init = function()
+    local group = vim.api.nvim_create_augroup("nvim_tree_auto_open", { clear = true })
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      group = group,
+      callback = function(data)
+        local path = data.file or ""
+        if path == "" or vim.fn.isdirectory(path) ~= 1 then
+          return
+        end
+
+        vim.cmd.cd(path)
+        require("lazy").load({ plugins = { "nvim-tree.lua" } })
+        require("nvim-tree.api").tree.open()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = group,
+      callback = function(data)
+        local path = data.file or ""
+        if path == "" or vim.fn.isdirectory(path) ~= 1 then
+          return
+        end
+
+        require("lazy").load({ plugins = { "nvim-tree.lua" } })
+        require("nvim-tree.api").tree.open()
+        pcall(vim.api.nvim_buf_delete, data.buf, { force = true })
+      end,
+    })
+  end,
   cmd = {
     "NvimTreeToggle",
     "NvimTreeFocus",
